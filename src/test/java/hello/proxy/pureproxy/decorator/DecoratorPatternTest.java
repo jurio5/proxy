47c35214b0f486cbe62684db904c0aa57840dc87
jurio5 <sia5492@naver.com>
2025-07-29 01:08:16 +0900

    [feat] 데코레이터 패턴 리팩토링 (추상 클래스로 중복 필드 해결 및 의존성 처리로 객체 생성 최소화)

--- src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -6,6 +6,8 @@ import hello.proxy.pureproxy.decorator.code.RealComponent;
 import hello.proxy.pureproxy.decorator.code.TimeDecorator;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @Slf4j
 public class DecoratorPatternTest {
@@ -29,10 +31,37 @@ public class DecoratorPatternTest {
     @Test
     void decorator2() {
         RealComponent realComponent = new RealComponent();
-        MessageDecorator messageDecorator = new MessageDecorator(realComponent);
-        TimeDecorator timeDecorator = new TimeDecorator(messageDecorator);
-        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
+        TimeDecorator timeDecorator = new TimeDecorator(realComponent);
+        MessageDecorator messageDecorator = new MessageDecorator(timeDecorator);
+        DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
 
         client.execute();
+    }
+
+    @Test
+    void decorator3() {
+        DecoConfiguration decoConfiguration = new DecoConfiguration();
+        DecoratorPatternClient client = new DecoratorPatternClient(decoConfiguration.messageDecorator());
+
+        client.execute();
+    }
+
+    @TestConfiguration
+    class DecoConfiguration {
+
+        @Bean
+        protected RealComponent realComponent() {
+            return new RealComponent();
+        }
+
+        @Bean
+        protected TimeDecorator timeDecorator() {
+            return new TimeDecorator(realComponent());
+        }
+
+        @Bean
+        protected MessageDecorator messageDecorator() {
+            return new MessageDecorator(timeDecorator());
+        }
     }
 }
---
+++ src/test/java/hello/proxy/pureproxy/decorator/code/AbstractDecorator.java
@@ -0,0 +1,9 @@
+package hello.proxy.pureproxy.decorator.code;
+
+public abstract class AbstractDecorator implements Component {
+    protected final Component component;
+
+    protected AbstractDecorator(Component component) {
+        this.component = component;
+    }
+}
