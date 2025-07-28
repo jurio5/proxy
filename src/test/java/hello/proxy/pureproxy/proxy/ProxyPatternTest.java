38169f7b57e8cf2ac2547a7af53e099770fb5f5d
jurio5 <sia5492@naver.com>
2025-07-29 00:54:13 +0900

    [feat] 프록시 패턴 리팩토링

--- src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
+++ src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -4,6 +4,8 @@ import hello.proxy.pureproxy.proxy.code.CacheProxy;
 import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
 import hello.proxy.pureproxy.proxy.code.RealSubject;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 
 public class ProxyPatternTest {
 
@@ -18,12 +20,25 @@ public class ProxyPatternTest {
 
     @Test
     void cacheProxyTest() {
-        RealSubject realSubject = new RealSubject();
-        CacheProxy cacheProxy = new CacheProxy(realSubject);
-        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
+        ProxyConfiguration proxyConfiguration = new ProxyConfiguration();
+        ProxyPatternClient client = new ProxyPatternClient(proxyConfiguration.cacheProxy());
 
         client.execute();
         client.execute();
         client.execute();
+    }
+
+    @TestConfiguration
+    class ProxyConfiguration {
+
+        @Bean
+        protected RealSubject realSubject() {
+            return new RealSubject();
+        }
+
+        @Bean
+        protected CacheProxy cacheProxy() {
+            return new CacheProxy(realSubject());
+        }
     }
 }
