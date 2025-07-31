0938bee381871213960274446d0a92e36d5e6bea
jurio5 <sia5492@naver.com>
2025-07-31 22:54:53 +0900

    [feat] 대상에 따른 프록시 팩토리 매핑 방식 (setProxyTargetClass을 이용한 CGLIB 매핑)

--- src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
+++ src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
@@ -1,6 +1,7 @@
 package hello.proxy.proxyfactory;
 
 import hello.proxy.common.advice.TimeAdvice;
+import hello.proxy.common.service.ConcreteService;
 import hello.proxy.common.service.ServiceImpl;
 import hello.proxy.common.service.ServiceInterface;
 import lombok.extern.slf4j.Slf4j;
@@ -30,6 +31,42 @@ public class ProxyFactoryTest {
         assertThat(AopUtils.isAopProxy(proxy)).isTrue(); // 프록시 팩토리로 만들어진 프록시만 프록시 여부를 확인할 수 있음
         assertThat(AopUtils.isJdkDynamicProxy(proxy)).isTrue(); // 프록시 팩토리로 만들어진 프록시만 프록시 여부를 확인할 수 있음
         assertThat(AopUtils.isCglibProxy(proxy)).isFalse(); // 프록시 팩토리로 만들어진 프록시만 프록시 여부를 확인할 수 있음
+    }
 
+    @Test
+    @DisplayName("구체 클래스만 있으면 CGLIB 사용")
+    void concreteProxy() {
+        ConcreteService target = new ConcreteService();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.addAdvice(new TimeAdvice());
+        ConcreteService proxy = (ConcreteService) proxyFactory.getProxy();
+
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+        proxy.call();
+
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue(); // 프록시 팩토리로 만들어진 프록시만 프록시 여부를 확인할 수 있음
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse(); // 프록시 팩토리로 만들어진 프록시만 프록시 여부를 확인할 수 있음
+        assertThat(AopUtils.isCglibProxy(proxy)).isTrue(); // 프록시 팩토리로 만들어진 프록시만 프록시 여부를 확인할 수 있음
+    }
+
+    @Test
+    @DisplayName("ProxyTargetClass 옵션을 사용하면 인터페이스가 있어도 CGLIB 를 사용하고, 클래스 기반 프록시 사용")
+    void proxyTargetClass() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(true); // 인터페이스가 있어도 CGLIB 로 매핑
+        proxyFactory.addAdvice(new TimeAdvice());
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+        proxy.save();
+
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue(); // 프록시 팩토리로 만들어진 프록시만 프록시 여부를 확인할 수 있음
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse(); // 프록시 팩토리로 만들어진 프록시만 프록시 여부를 확인할 수 있음
+        assertThat(AopUtils.isCglibProxy(proxy)).isTrue(); // 프록시 팩토리로 만들어진 프록시만 프록시 여부를 확인할 수 있음
     }
 }
