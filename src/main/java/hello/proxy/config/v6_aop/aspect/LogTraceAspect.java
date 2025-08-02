09371511212b6ab411723601f4d8f8f51d0c4940
jurio5 <sia5492@naver.com>
2025-08-03 00:14:39 +0900

    [feat] Spirng aop Aspect Annotation & Around Pointcut

---
+++ src/main/java/hello/proxy/config/v6_aop/aspect/LogTraceAspect.java
@@ -0,0 +1,35 @@
+package hello.proxy.config.v6_aop.aspect;
+
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Slf4j
+@Aspect
+@RequiredArgsConstructor
+public class LogTraceAspect {
+
+    private final LogTrace logTrace;
+
+    @Around("execution(* hello.proxy.app..*(..))") // 포인트컷
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable { // 어드바이스
+        TraceStatus status = null;
+        try {
+            String message = joinPoint.getSignature().toShortString();
+            status = logTrace.begin(message);
+
+            // 로직 호출
+            Object result = joinPoint.proceed();
+
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
